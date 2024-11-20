eaced9247516ea9f4228c00241b080c2b0fba117
hamzalgh <laghrissi18hamza@gmail.com>
2024-11-20 20:38:21 +0000

    Convert to functional component

--- my-app/src/components/movie.jsx
+++ my-app/src/components/movie.jsx
@@ -1,23 +1,20 @@
-import { Component } from "react"
 
-class Movie extends Component{
-    render(){
-        return(
-            <>
-            <div className="card-container">
-                {this.props.data.map((list)=>{
-                    return (
-                    <div key={list.id} className="card">
-                        <img src={`https://image.tmdb.org/t/p/w200/${list.poster_path}`} alt={list.title} />
-                        <div className="card-content">
-                            <h3 className='card-title'>{list.title}</h3>
-                        </div>
+function Movie(props){
+    return(
+        <>
+        <div className="card-container">
+            {props.data.map((list)=>{
+                return (
+                <div key={list.id} className="card">
+                    <img src={`https://image.tmdb.org/t/p/w200/${list.poster_path}`} alt={list.title} />
+                    <div className="card-content">
+                        <h3 className='card-title'>{list.title}</h3>
                     </div>
-                    )
-                })}
-            </div>
-            </>
-        )
-    }
+                </div>
+                )
+            })}
+        </div>
+        </>
+    )
 }
 export default Movie
